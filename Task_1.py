cad09933f9f3beebcd98d473fd583cb00e494b12
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-29 01:26:41 +0200

    Додала зчитувача словника. Вийшло якось не красиво, мені здається ви давали красивіший спосіб. Думаю на 3 коміт спробую усе красиво проциклити

--- Task_1.py
+++ Task_1.py
@@ -3,6 +3,13 @@ def evaluate_polynomial(poly_dict, x):
     for key in poly_dict.keys():
         sum = sum+pow(x, key)*poly_dict[key]
     return sum
-my_poly = {0: -10, 2: 3, 4: 1}
-x=2
+my_poly = {}
+x=int(input())
+while True:
+    try:
+        c_key, c_val=map(int, input().split())
+        my_poly[c_key] = c_val
+    except ValueError: break
+
+
 print(evaluate_polynomial(my_poly, x))
