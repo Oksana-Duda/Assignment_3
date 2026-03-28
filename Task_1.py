44a245741b18513c74250120f177db81e6b99e4b
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-29 01:07:54 +0200

    Перевіряю роботу функції для конкретного значення

--- Task_1.py
+++ Task_1.py
@@ -3,3 +3,6 @@ def evaluate_polynomial(poly_dict, x):
     for key in poly_dict.keys():
         sum = sum+pow(x, key)*poly_dict[key]
     return sum
+my_poly = {0: -10, 2: 3, 4: 1}
+x=2
+print(evaluate_polynomial(my_poly, x))
