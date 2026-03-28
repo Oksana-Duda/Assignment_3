0d5456443a4adfedace8f5a2d146d82b61bf99aa
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-29 01:49:39 +0200

    Функція проходиться по студентах і їх оцінках, визначають, чи оцінки менші за певну межу. Використовую презумкцію невинуватості, тобто припускаю, що студент хороший, поки його оцінки не показують протилежне

---
+++ Task_2.py
@@ -0,0 +1,7 @@
+def get_succesful_students(student_list, passing_grade=60):
+    suc_st = []
+    for student in student_list:
+        cnt = True
+        for value in student["scores"].values():
+            if value < passing_grade:
+                cnt = False
