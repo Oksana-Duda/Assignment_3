0741e2b5138748e78799e58d5035261a683ca028
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-04-01 00:09:30 +0300

    Доробила функцію: тепер словник заповнений даними, створила назви глав словника згідно тз

--- Task 3.py
+++ Task 3.py
@@ -2,3 +2,11 @@ def analyze_methodes(data):
     res_data={}
     for i in data:
         res_data[data["method"]]={}
+        res_data[data["method"]]["max_error"]=0
+        res_data[data["method"]]["iterations_count"]=0
+        res_data[data["method"]]["total_time_ms"]=0
+    for i in data:
+        res_data[data["method"]["max_error"]]=max(res_data[data["method"]["max_error"]], data["error"])
+        res_data[data["method"]["iterations_count"]] = max(res_data[data["method"]["iterations_count"]], data["iteration"])
+        res_data[data["method"]["total_time_ms"]] = max(res_data[data["method"]["total_time_ms"]], data["time_ms"])
+    return res_data
